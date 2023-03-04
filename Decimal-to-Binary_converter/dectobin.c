2ccd3d3d496e76ac341e3fe4a1e0605d1b3166eb
raghib01 <rakibulislamragib2@gmail.com>
2023-03-05 00:35:58 +0600

    commit message

---
+++ Decimal-to-Binary_converter/dectobin.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void main()
+{
+    int dec;
+    printf("Input a Decimal number: ");
+    scanf("%d", &dec);
+    int rem, i = 0;
+    int array[900000];
+
+    while (dec)
+    {
+        rem = dec % 2;
+        dec = dec / 2;
+        array[i] = rem;
+        i++;
+    }
+    printf("Binary number is: ");
+    for (int j = i - 1; j >= 0; j--)
+    {
+        printf("%d", array[j]);
+    }
+    printf("\n");
+}
