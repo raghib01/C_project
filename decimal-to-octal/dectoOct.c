b62cac523c10b4565f8f23e1afd0a8442e69059c
raghib01 <rakibulislamragib2@gmail.com>
2023-03-05 00:34:17 +0600

    commit message

---
+++ decimal-to-octal/dectoOct.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void main()
+{
+    int oct;
+    printf("Input a Decimal number: ");
+    scanf("%d", &oct);
+    int rem, i = 0;
+    int array[900000];
+
+    while (oct)
+    {
+        rem = oct % 8;
+        oct = oct / 8;
+        array[i] = rem;
+        i++;
+    }
+    printf("Octal number is: ");
+    for (int j = i - 1; j >= 0; j--)
+    {
+        printf("%d", array[j]);
+    }
+    printf("\n");
+}
